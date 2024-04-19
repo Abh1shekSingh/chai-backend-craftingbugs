bc8e4c4f5936eeed2ca76978b4255f7a857c60b9
Abhishek Singh <abhinav210702@gmail.com>
2024-04-19 22:05:10 +0530

    Error Related Files Manifested

---
+++ src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+    constructor(
+        statusCode, 
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message) 
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.stack = stack;
+        this.success = false;
+        this.data = null;
+        
+    }
+}
+
+export { ApiError };
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+
+const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    }
+}
+
+export { asyncHandler };
+
+/*
+const asyncHandler = (fn) => async(req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch(err) {
+        res.send(err.code || 500).json({
+            success: false,
+            message: err.message || "Internal Server Error"
+        })
+    }
+}
+*/
