6e4e6ef85ea166885b68c3543827c9d69dd2effa
Abhishek Singh <abhinav210702@gmail.com>
2024-04-20 15:40:09 +0530

    Created the Route and controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 const app = express();
 
 app.use(express.json({limit: '16kb'}))
@@ -12,5 +12,10 @@ app.use(cors({
 app.use(express.urlencoded({extended: true, limit:'16kb'}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+// Importing the Routes
+import userRouter from "./routes/user.route.js"
+
+app.use("/api/v1/user", userRouter)
 
 export { app }
